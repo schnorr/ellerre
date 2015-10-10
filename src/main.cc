b3e923190401a4f4068bc9b6f7b0daeac22c7218
Lucas Mello Schnorr <schnorr@inf.ufrgs.br>
2015-10-10 00:54:08 -0300

    make it print in the main function instead

--- src/main.cc
+++ src/main.cc
@@ -27,8 +27,24 @@ int main (int argc, char **argv)
   std::cout << *grammar << std::endl;
 
   std::cout << std::endl << "First sets:" << std::endl;
-  
-  grammar->first();
+  std::map<Symbol*,std::set<Symbol*> > firsts = grammar->first();
+  for (auto& entry : firsts){
+    std::cout << *(entry.first) << " -- ";
+    for (Symbol *s : entry.second){
+      std::cout << *s << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  std::cout << std::endl << "Follow sets:" << std::endl;
+  std::map<Symbol*,std::set<Symbol*> > follows = grammar->follow();
+  for (auto& entry : follows){
+    std::cout << *(entry.first) << " -- ";
+    for (Symbol *s : entry.second){
+      std::cout << *s << " ";
+    }
+    std::cout << std::endl;
+  }
   
   delete grammar;
   grammar = NULL;
