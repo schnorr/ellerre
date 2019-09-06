f96aeb29164af0e09475dc39960b3740231d589c
Marcelo Miletto <marcelo.miletto@inf.ufrgs.br>
2019-09-06 12:22:21 -0300

    main for build a LR0 automata given an input grammar

---
+++ src/lr0.cc
@@ -0,0 +1,40 @@
+/*
+    This file is part of Ellerre
+
+    Ellerre is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ellerre is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Public License for more details.
+
+    You should have received a copy of the GNU Public License
+    along with Ellerre. If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "main.h"
+
+Grammar *grammar = NULL;
+
+int main (int argc, char **argv)
+{
+  grammar = new Grammar();
+  Parser* parser;
+
+  int ret = yyparse();
+
+  grammar->expand_grammar();
+  parser = new Parser("LR0", grammar);
+  parser->LR0();
+  
+  std::cout << *grammar << std::endl;
+  parser->print_item_set();
+  parser->print_automata();
+
+  delete parser;
+  delete grammar;
+  grammar = NULL;
+  return ret;
+}
