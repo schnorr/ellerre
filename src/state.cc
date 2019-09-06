081a9bc7738b0a615bdec3285896c1e17e0a6557
Marcelo Miletto <marcelo.miletto@inf.ufrgs.br>
2019-09-06 12:20:02 -0300

    Initializers for State class

--- src/state.cc
+++ src/state.cc
@@ -16,3 +16,52 @@
 */
 
 #include "state.h"
+
+State::State()
+{
+}
+
+State::State(std::set<Item*> kernel)
+{
+  this->kernel = kernel;
+}
+
+State::State(int id,
+             std::set<Item*> kernel, 
+             std::set<Item*> item_set)
+{
+  this->id = id;
+  this->kernel = kernel;
+  this->item_set = item_set;
+  set_union(this->kernel.begin(), this->kernel.end(), 
+            this->item_set.begin(), this->item_set.end(), 
+            std::inserter(this->all_items, this->all_items.begin()));
+}
+
+State::~State()
+{
+  this->kernel.clear();
+  this->item_set.clear();
+  this->transitions.clear();
+}
+ 
+void State::setId(int id)
+{
+  this->id = id;
+}
+
+void State::setItemSet(std::set<Item*> item_set)
+{
+  this->item_set = item_set;
+  set_union(this->kernel.begin(), this->kernel.end(), 
+            this->item_set.begin(), this->item_set.end(), 
+            std::inserter(this->all_items, this->all_items.begin()));
+}
+
+void State::setKernel(std::set<Item*> kernel)
+{
+  this->kernel = kernel;
+  set_union(this->kernel.begin(), this->kernel.end(), 
+            this->item_set.begin(), this->item_set.end(), 
+            std::inserter(this->all_items, this->all_items.begin()));
+}
