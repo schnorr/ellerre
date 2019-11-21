2ab7c6918911ab4397cc78ea0d8752caf0562a23
Marcelo Miletto <marcelocm97@hotmail.com>
2019-11-21 00:38:09 -0300

    different main for different parsers

---
+++ src/main_lr1.cc
@@ -0,0 +1,40 @@
+/*
+    This file is part of Ellerre
+
+    Ellerre is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ellerre is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Public License for more details.
+
+    You should have received a copy of the GNU Public License
+    along with Ellerre. If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "main.h"
+
+Grammar *grammar = NULL;
+
+int main (int argc, char **argv)
+{
+  grammar = new Grammar();
+  LR1* parser;
+
+  int ret = yyparse();
+
+  grammar->expand_grammar();
+  parser = new LR1(grammar);
+
+  std::cout << *grammar << std::endl;
+  parser->print_item_set();
+  parser->print_automata();
+
+  delete grammar;
+  delete parser;
+  grammar = NULL;
+  parser = NULL;
+  return ret;
+}
