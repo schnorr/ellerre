f21e40391dd38685d74e5281221b9d5c8d02a9f6
Ana <analuisavs10@gmail.com>
2021-06-12 20:21:41 -0300

    previous, next and reset

--- runanimation.py
+++ runanimation.py
@@ -7,6 +7,8 @@ from PIL import Image, ImageTk
  
 count = 0
 
+# TO-DO Pintar os estados de branco ao inves de gerar arquivo apenas com parte dos steps
+# It didn't go very well
 def create_tmp_dots(file_name, tmp_dir):
     write = True
     step=0
@@ -27,7 +29,6 @@ def create_tmp_dots(file_name, tmp_dir):
                     f1.write(line) 
         step = step + 1 
 
-
 def create_images(tmp_dir):
     id = 0
     for file in os.listdir(tmp_dir):
@@ -35,12 +36,31 @@ def create_images(tmp_dir):
         os.system(convert_line)
         id = id + 1
 
+# TO-DO 
+# [X] Ajustar botões
+# [ ] Ajustar img resizing
+# [ ] Marcador para mostrar os steps
+# [ ] Testar com todos os casos
 class MainCanva(tk.Tk):
     
     def __init__(self, *args, **kwargs):
 
         tk.Tk.__init__(self, *args, **kwargs)
         self.title('EllErre - Animation')
+
+
+        self.menu = tk.Menu(self)
+        self.config(menu=self.menu)
+
+        # To create a menu
+        fileMenu = tk.Menu(self.menu)
+        fileMenu.add_command(label="Load .dot File")
+        fileMenu.add_command(label="Save current image")
+        fileMenu.add_command(label="Exit", command=self.close_app)
+        self.menu.add_cascade(label="File", menu=fileMenu)
+        self.menu.add_command(label="Previous", command=self.on_click_previous, state=tk.DISABLED)
+        self.menu.add_command(label="Next", command=self.on_click_next)
+        self.menu.add_command(label="Reset", command=self.on_click_reset)
 
         # Create temporary images
         file_name = sys.argv[1]
@@ -53,7 +73,7 @@ class MainCanva(tk.Tk):
         self.canvas = tk.Canvas(self, bg='white')
 
         # Need to define it based on the maximum size of the image
-        self.canvas.config(width=1100, height=620)
+        self.canvas.config(width=1150, height=600)
         self.canvas.pack(fill=tk.BOTH, expand=1)
         self.canvas.bind("<Configure>", self.resize)
         self.first_image = self.tmp_dir.name + "/0-out.png"
@@ -63,42 +83,46 @@ class MainCanva(tk.Tk):
         # self.img = tk.PhotoImage(file=self.first_image)
         self.myimg = self.canvas.create_image((0, 0), image=self.img, anchor="nw")
 
-        # Load canva layout images
-        button_img_next = tk.PhotoImage(file=r'./img/next.png')
-        button_img_reset = tk.PhotoImage(file=r'./img/reset.png')
-
-        # Load and add buttons to canva
-        self.button1 = tk.Button(self, image=button_img_next, command=self.on_click_next)
-        self.button1.config(width=1100, height=50)
-        self.button1.pack()
-
-        button2 = tk.Button(self, image=button_img_reset, command=self.on_click_reset)
-        button2.config(width=1100, height=30)
-        button2.pack()
-
         # Open the canva
         self.mainloop()
-        # self.tmp_dir.cleanup()
+        self.tmp_dir.cleanup()
+
+    def on_click_previous(self):
+        global img, count
+        count = count - 1
+        filename = self.tmp_dir.name + '/' + str(count) + '-out.png'
+
+        # Check if thereis a previous image, if its the first,
+        #  disable the previous button
+        if(count > 0):
+            img = tk.PhotoImage(file=filename)
+            self.menu.entryconfig(2, state=tk.NORMAL)
+        else:
+            img = tk.PhotoImage(file=filename)
+            self.menu.entryconfig(2, state=tk.DISABLED)
+        
+        self.canvas.itemconfig(self.myimg, image = img)
 
     def on_click_next(self):
         global img, count
         count = count + 1
         filename = self.tmp_dir.name + '/' + str(count) + '-out.png'
 
-        # Check if thereis a next image, if not disable button
+        # Check if there is a next image, if not disable button
         if(os.path.exists(filename)):
             img = tk.PhotoImage(file=filename)
+            self.menu.entryconfig(2, state=tk.NORMAL)
+            self.canvas.itemconfig(self.myimg, image = img)
         else:
-            self.button1["state"] = "disable"
-            print("File not accessible")
-        self.canvas.itemconfig(self.myimg, image = img)
+            self.menu.entryconfig(3, state=tk.DISABLED)
 
     def on_click_reset(self):
         global img, count
-        self.button1["state"] = "active"
+        count=0
+        self.menu.entryconfig(3, state=tk.NORMAL)
+        self.menu.entryconfig(2, state=tk.DISABLED)
         img = tk.PhotoImage(file=self.first_image)
         self.canvas.itemconfig(self.myimg, image = img)
-        count=0
 
     def resize(self, event):
         global count
